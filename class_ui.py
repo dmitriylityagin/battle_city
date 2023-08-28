9bd9a46b7ddd160de4c2b3a48e3a89e26a6208b2
D L <mass17@pm.me>
2023-08-28 21:09:29 +0300

    ok

--- class_ui.py
+++ class_ui.py
@@ -1,8 +1,9 @@
 import pygame as pg
 from game_object import objects
-from framedraw import window
+from framedraw import window, WIDTH, HEIGHT, TILE
 
 fontUI = pg.font.Font(None,32)
+fontBig = pg.font.Font(None, 70)
 
 class UI:
     def __init__(self):
@@ -16,9 +17,23 @@ class UI:
             if obj.type == 'tank':
                 pg.draw.rect(window, obj.color, (5 + i * 70, 5, 22, 22))
 
+                text = fontUI.render(str(obj.rank), 1, 'black')
+                rect = text.get_rect(center=(5 + i * 70 + 11, 5 + 11))
+                window.blit(text, rect)
+
                 text = fontUI.render(str(obj.hp), 1, obj.color)
-                rect = text.get_rect(center=(5 + i * 70 + 32, 5 + 11))
+                rect = text.get_rect(center=(5 + i * 70 + TILE, 5 + 11))
                 window.blit(text, rect)
                 i += 1
+        t = 0
+        for obj in objects:
+            if obj.type == 'tank':
+                t += 1
+                tankWin = obj
 
+        if t == 1:
+            text = fontBig.render('ПОБЕДИЛ', 1, 'white')
+            rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 100))
+            window.blit(text, rect)
 
+            pg.draw.rect(window, tankWin.color, (WIDTH // 2 - 100, HEIGHT // 2, 200, 200))
